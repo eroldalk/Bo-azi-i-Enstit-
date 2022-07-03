5402bcc9542380bb1a2353e6e594b8c625cdeeda
Erol Dalkılıç <eroldalg@gmail.com>
2022-07-03 14:27:08 +0300

    adding

---
+++ 4.DERS/if-else.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	
+	int a;
+	
+	
+	cout<<"bir sayi girin"<<endl;
+	cin>>a;
+	
+	if(a>=60)
+	{
+	
+	cout<<"Gecti"<<endl;
+
+	
+	}
+	else 
+	cout<<"Kaldi";
+	
+	
+	
+	
+}
---
+++ 4.DERS/kararlıyapı4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	
+	int plaka;
+	
+	cout<<"bir plaka degeri giriniz"<<endl;
+	cin>>plaka;
+	
+	if(plaka==34)
+	{
+		cout<<"Istanbul";
+	}
+	
+	else if (plaka==21) 
+	{
+		cout<<"Diyarkabir";
+	}
+	
+	else if (plaka==20) 
+	{
+		cout<<"Denizli";
+	}
+	
+	else if (plaka==35) 
+	{
+		cout<<"Ýzmir";
+	}
+	
+	else if (plaka==01) 
+	{
+		cout<<"Adana";
+	}
+	
+	else 
+		cout<<"Bulunamadi";
+
+	
+
+	
+}
